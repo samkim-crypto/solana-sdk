04f8afb7bc0c78f373df2e7694b6a1958a5bfa8c
samkim-crypto <skim13@cs.stanford.edu>
2025-05-11 10:54:44 +0900

    reformulate visibility for bpf

--- bls/src/lib.rs
+++ bls/src/lib.rs
@@ -22,7 +22,6 @@ use {
 // TODO: add conversion between compressed and uncompressed representation of
 // signatures, pubkeys, and proof of possessions
 
-#[cfg(not(target_os = "solana"))]
 pub mod error;
 #[cfg(not(target_os = "solana"))]
 pub mod keypair;
