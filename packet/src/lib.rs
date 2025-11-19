f7ca3a7bf9da9ce3ef1994acd68274a2e457d131
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-11-20 03:38:20 +0800

    packet: add constructor to Meta (#444)
    * add constructor to Meta

--- packet/src/lib.rs
+++ packet/src/lib.rs
@@ -243,6 +243,22 @@ impl PartialEq for Packet {
 }
 
 impl Meta {
+    pub fn new(
+        size: usize,
+        addr: IpAddr,
+        port: u16,
+        flags: PacketFlags,
+        remote_pubkey: Option<Pubkey>,
+    ) -> Self {
+        Self {
+            size,
+            addr,
+            port,
+            flags,
+            remote_pubkey: remote_pubkey.unwrap_or_default(),
+        }
+    }
+
     pub fn socket_addr(&self) -> SocketAddr {
         SocketAddr::new(self.addr, self.port)
     }
@@ -384,5 +400,26 @@ mod tests {
         let pubkey = Pubkey::default();
         meta.set_remote_pubkey(pubkey);
         assert!(meta.remote_pubkey().is_none());
+    }
+
+    #[test]
+    fn test_meta_new() {
+        let size = 1024;
+        let addr = IpAddr::V4(Ipv4Addr::new(192, 168, 1, 1));
+        let port = 8080;
+        let flags = PacketFlags::FROM_STAKED_NODE | PacketFlags::REPAIR;
+        let pubkey = Pubkey::new_unique();
+
+        let meta = Meta::new(size, addr, port, flags, Some(pubkey));
+
+        assert_eq!(meta.size, size);
+        assert_eq!(meta.addr, addr);
+        assert_eq!(meta.port, port);
+        assert_eq!(meta.flags, flags);
+        assert_eq!(meta.remote_pubkey, pubkey);
+        assert_eq!(meta.remote_pubkey(), Some(pubkey));
+        let meta = Meta::new(size, addr, port, flags, None);
+        assert_eq!(meta.remote_pubkey, Pubkey::default());
+        assert_eq!(meta.remote_pubkey(), None);
     }
 }
