ca5219fa5f09b58a8faaa6cecb55ca9e0374b63c
samkim-crypto <skim13@cs.stanford.edu>
2025-12-14 18:56:32 +0400

    add clarifying comments on hashing

--- bls-signatures/src/hash.rs
+++ bls-signatures/src/hash.rs
@@ -7,11 +7,17 @@ use {crate::proof_of_possession::POP_DST, blstrs::G2Projective};
 pub const HASH_TO_POINT_DST: &[u8] = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
 
 /// Hash a message to a G2 point for signature generation and verification
+///
+/// If hashing a payload for a Proof-of-Possession (PoP), use
+/// `hash_pop_payload_to_point` instead.
 pub fn hash_signature_message_to_point(message: &[u8]) -> G2Projective {
     G2Projective::hash_to_curve(message, HASH_TO_POINT_DST, &[])
 }
 
 /// Hash a message to a G2 point for proof-of-possession generation and verification
+///
+/// If hashing a message for a standard BLS signature, use
+/// `hash_signature_message_to_point` instead.
 pub(crate) fn hash_pop_payload_to_point(payload: &[u8]) -> G2Projective {
     G2Projective::hash_to_curve(payload, POP_DST, &[])
 }
