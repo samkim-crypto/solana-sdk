77fb9b2d88be88a5460238052aff22ef80db72fa
Joe C <joe.caulfield@anza.xyz>
2025-12-20 07:38:42 -0500

    reward-info: change `commission` to `commission_bps` (#500)

--- reward-info/src/lib.rs
+++ reward-info/src/lib.rs
@@ -40,6 +40,7 @@ pub struct RewardInfo {
     pub lamports: i64,
     /// Account balance in lamports after `lamports` was applied
     pub post_balance: u64,
-    /// Vote account commission when the reward was credited, only present for voting and staking rewards
-    pub commission: Option<u8>,
+    /// Vote account commission in basis points when the reward was credited,
+    /// only present for voting and staking rewards.
+    pub commission_bps: Option<u16>,
 }
