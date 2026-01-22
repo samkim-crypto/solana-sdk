bb8e4309856793def29c6c020438184f6480f61e
puhtaytow <18026645+puhtaytow@users.noreply.github.com>
2026-01-22 10:48:54 +0100

    docs: add StableAbi docs to frozen-abi (#529)
    * add stableabi docs
    
    * fix formatting

--- frozen-abi/src/lib.rs
+++ frozen-abi/src/lib.rs
@@ -1,3 +1,72 @@
+//! # StableAbi
+//!
+//! The `StableAbi` is an optional extension to `frozen-abi` that provides functionality for
+//! detecting unintended encoding changes. It is designed to be used in conjunction with the
+//! mandatory `AbiExample`.
+//!
+//! ## How it works?
+//!
+//! When you annotate a type with:
+//!
+//! ```rust,ignore
+//! #[frozen_abi(abi_digest = "...")]
+//! struct MyType { ... }
+//! ```
+//!
+//! The macro would generate the `test_abi_digest` test that verifies binary layout stability:
+//! - Initializes a deterministic random number generator with fixed seed
+//! - Generates 10_000 instances of the type via `StableAbi::random()`
+//! - Serializes each instance with bincode
+//! - Hashes all serialized bytes together
+//! - Compares the resulting hash against the provided in `abi_digest` attribute
+//!
+//! Using the seeded RNG ensures same sequence of random values across runs, while the 10_000
+//! iterations brings a wide range of value combinations. By hashing serialized bytes, changes
+//! to padding, endianness or encoding are detected.
+//!
+//!
+//! ## Adding StableAbi to a New Type
+//!
+//! For types which implement `Distribution<T>` for `StandardUniform`:
+//!
+//! ```rust,ignore
+//! #[derive(StableAbi)]
+//! #[frozen_abi(abi_digest = "...")]
+//!    struct MyType { ... }
+//! ```
+//!
+//! For types which don't, you must provide as well a custom implementation:
+//!
+//! ```rust,ignore
+//! #[cfg(feature = "frozen-abi")]
+//! impl solana_frozen_abi::rand::prelude::Distribution<MyType>
+//!     for solana_frozen_abi::rand::distr::StandardUniform
+//! {
+//!     fn sample<R: solana_frozen_abi::rand::Rng + ?Sized>(&self, rng: &mut R) -> MyType {
+//!            MyType {
+//!                field: rng.random(),
+//!                ...
+//!            }
+//!        }
+//!    }
+//! ```
+//!
+//! Deriving the `StableAbi` adds the following impl, which comes with the default `random()`
+//! implementation:
+//!
+//! ```rust,ignore
+//! impl ::solana_frozen_abi::stable_abi::StableAbi for MyType {}
+//! ```
+//!
+//! ## Edge Cases
+//!
+//! 1. It will not detect field name or order changes, nor same size type swaps (e.g., `i64`
+//!    and `u64`). These cases are still covered by `AbiExample`
+//! 2. The implementor must ensure a consistent order of `rng.random()` calls, as any change
+//!    will result in different hash
+//! 3. For collection types with non deterministic ordering (e.g., `HashMap`), it is recommended
+//!    to insert only one item to avoid false positives caused by iteration order differences
+
 #![allow(incomplete_features)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(feature = "frozen-abi", feature(specialization))]
