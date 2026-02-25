9d6e9e83107f02416aff9a75678ae60fd5d84488
Gabe Rodriguez <grod220@gmail.com>
2026-02-25 10:49:07 +0100

    pod!: Migrate `ListView` to standalone crate (#178)
    Standalone spl-list-view crate

--- pod/src/list/mod.rs
+++
@@ -1,9 +0,0 @@
-mod list_trait;
-mod list_view;
-mod list_view_mut;
-mod list_view_read_only;
-
-pub use {
-    list_trait::List, list_view::ListView, list_view_mut::ListViewMut,
-    list_view_read_only::ListViewReadOnly,
-};
