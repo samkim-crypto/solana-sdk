5c3720d8d8bf4fe12f8d583d69cba05164d52bbf
Minh Vu <vuhoangminh97@gmail.com>
2026-06-17 12:53:46 +0200

    Fix AccountKeys equality for different lengths (#767)
    Fix AccountKeys equality length check

--- message/src/account_keys.rs
+++ message/src/account_keys.rs
@@ -1,7 +1,7 @@
 use {
     crate::{compiled_instruction::CompiledInstruction, v0::LoadedAddresses, CompileError},
     alloc::{collections::BTreeMap, vec::Vec},
-    core::{iter::zip, ops::Index},
+    core::ops::Index,
     solana_address::Address,
     solana_instruction::Instruction,
 };
@@ -145,7 +145,7 @@ impl<'a> AccountKeys<'a> {
 
 impl PartialEq for AccountKeys<'_> {
     fn eq(&self, other: &Self) -> bool {
-        zip(self.iter(), other.iter()).all(|(a, b)| a == b)
+        self.iter().eq(other.iter())
     }
 }
 
@@ -230,6 +230,45 @@ mod tests {
         let account_keys = AccountKeys::new(&static_keys, Some(&dynamic_keys));
 
         assert!(account_keys.iter().eq(keys.iter()));
+    }
+
+    #[test]
+    fn test_eq() {
+        let keys = test_account_keys();
+
+        let static_keys = vec![keys[0], keys[1], keys[2]];
+        let split_static_keys = vec![keys[0]];
+        let split_dynamic_keys = LoadedAddresses {
+            writable: vec![keys[1]],
+            readonly: vec![keys[2]],
+        };
+
+        assert_eq!(
+            AccountKeys::new(&static_keys, None),
+            AccountKeys::new(&split_static_keys, Some(&split_dynamic_keys)),
+        );
+    }
+
+    #[test]
+    fn test_ne_with_different_lengths() {
+        let keys = test_account_keys();
+
+        let short_static_keys = vec![keys[0]];
+        let long_static_keys = vec![keys[0], keys[1]];
+        let long_dynamic_keys = LoadedAddresses {
+            writable: vec![keys[1]],
+            readonly: vec![],
+        };
+
+        let short_account_keys = AccountKeys::new(&short_static_keys, None);
+        let long_static_account_keys = AccountKeys::new(&long_static_keys, None);
+        let long_dynamic_account_keys =
+            AccountKeys::new(&short_static_keys, Some(&long_dynamic_keys));
+
+        assert_ne!(short_account_keys, long_static_account_keys);
+        assert_ne!(long_static_account_keys, short_account_keys);
+        assert_ne!(short_account_keys, long_dynamic_account_keys);
+        assert_ne!(long_dynamic_account_keys, short_account_keys);
     }
 
     #[test]
