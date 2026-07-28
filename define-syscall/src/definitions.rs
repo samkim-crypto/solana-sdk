23dd55224d3ec783ee88edc0d14bab230f3bb526
zz-sol <allaboutshop10@163.com>
2026-07-27 21:33:06 -0400

    [define_syscall] add keccak_p (#838)
    add define syscall for keccak_p

--- define-syscall/src/definitions.rs
+++ define-syscall/src/definitions.rs
@@ -20,6 +20,7 @@ define_syscall!(fn sol_create_program_address(seeds_addr: *const u8, seeds_len: 
 define_syscall!(fn sol_try_find_program_address(seeds_addr: *const u8, seeds_len: u64, program_id_addr: *const u8, address_bytes_addr: *const u8, bump_seed_addr: *const u8) -> u64);
 define_syscall!(fn sol_sha256(vals: *const u8, val_len: u64, hash_result: *mut u8) -> u64);
 define_syscall!(fn sol_keccak256(vals: *const u8, val_len: u64, hash_result: *mut u8) -> u64);
+define_syscall!(fn sol_keccak_p1600(state: *mut u64, rounds: u64) -> u64);
 define_syscall!(fn sol_blake3(vals: *const u8, val_len: u64, hash_result: *mut u8) -> u64);
 define_syscall!(fn sol_sha512(vals: *const u8, val_len: u64, hash_result: *mut u8) -> u64);
 define_syscall!(fn sol_curve_validate_point(curve_id: u64, point_addr: *const u8, result: *mut u8) -> u64);
